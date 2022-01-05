24cdc6ac62789ed43e9daf0e238cec77c191e684
vlad <you@example.com>
2022-01-05 22:59:03 +0300

    new arnold

--- src/js/analyzeGraph/arnold.js
+++ src/js/analyzeGraph/arnold.js
@@ -3,29 +3,29 @@ import Fraction from "../Fraction";
 
 
 function arnold(target, lastTarget, power){
+    console.log("Арнольд");
     let cycle = exactCycle(target, lastTarget);
     cycle = cycle.slice(1, cycle.length - 1);
     let lastTargetPower = lastTarget.power.clone();
+    let sideInSum = new Fraction(0);
+
 
     for (let i = 0; i < cycle.length; i++) {
         let line = cycle[i];
-        // if(line.children.length > 1){
-        //     for (let j = i; j < cycle.length; j++) {
-        //         console.log("after divide",cycle[j].power.divide(line.children.length).getStr());
-        //     }
-        // }
         if(line.sideIn.length > 0){
             let sum = new Fraction(0);
             for (let j = 0; j < line.sideIn.length; j++) {
                 sum.plus(line.sideIn[j].power);
-                console.log("SUMM", sum.getStr());
+                sideInSum.plus(line.sideIn[j].power);
             }
             for (let j = i; j < cycle.length; j++) {
-
-                console.log(cycle[j].power.getStr(), " minus ", sum.getStr());
-                cycle[j].power.minus(sum.getNum() * 2, sum.getDet());
-                cycle[j].line.classList.add("a5");
-                console.log("FINAL", cycle[j].power.getStr());
+                cycle[j].power.minus(sum);
+            }
+        }
+        if(line.children.length > 1){
+            for (let j = i; j < cycle.length; j++) {
+                //console.log("after divide",cycle[j].power.divide(line.children.length).getStr());
+                sideInSum.divide(2);
             }
         }
     }
@@ -39,12 +39,22 @@ function arnold(target, lastTarget, power){
     //Вычитаем из общей мощности переданную от входящей грани, потому что сейчас будет арнольд, а не простое сложение
     //target.power.minus(power.getNum(), power.getDet());
 
-    console.log("Арнольд");
+
     console.log("target power: ", target.power.getStr());
     console.log("income power: ", power.getStr());
-
-    let x = target.power.clone().divide(target.power.clone().divide(power).minus(1));
-    target.power.plus(x); //Меняем мощность у текущей грани, арнольд готов
+    //1.5
+    let kx = target.power.clone().divide(power).minus(1);
+    //2
+    console.log('sideInSum', sideInSum.getStr());
+    target.power.plus(sideInSum);
+    //3
+    let tmp = target.power.clone().divide(power);
+    //4
+    let x = tmp.clone().divide(kx);
+    //5
+    power.multiply(x);
+    //6
+    target.power.plus(power); //Меняем мощность у текущей грани, арнольд готов
 
     console.log("x", x.getStr());
     console.log("final target power", target.power.getStr());
